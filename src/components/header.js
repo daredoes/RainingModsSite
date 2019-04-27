3df799180b0efc3e3f51bc8145f4010e2c9c0ec6
Daniel Evans <me@daredoes.work>
2019-04-26 23:12:34 -0700

    fix build error

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ class Header extends React.Component {
         {(globalState) => { 
           const hasRootFolder = globalState.user && globalState.user.rootFolder;
           const noUserElement = <p className="is-6 has-text-danger subtitle">Download our <a href="#">Automatic Mod Installer</a>, or turn it on to <Link to="/second">supercharge</Link> the website!</p>;
-          const hasRootFolderElement = <p className="is-6 has-text-success subtitle">Risk of Rain 2 {globalState.user.has_bepin && 'and BepInEx'} located at <a onClick={() => {
+          const hasRootFolderElement = <p className="is-6 has-text-success subtitle">Risk of Rain 2 {globalState.user && globalState.user.has_bepin && 'and BepInEx'} located at <a onClick={() => {
             const newPath = prompt('Enter the path to the folder where Risk of Rain 2.exe is found.\nThis is located in your Steam folder under "steamapps/common".', globalState.user.rootFolder || "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2");
             if (newPath) {
               globalState.updateRootFolder(newPath)
