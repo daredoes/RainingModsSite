e44c9fe260ec54ff221b853925907ea5e78294e9
Daniel Evans <me@daredoes.work>
2019-04-26 23:34:12 -0700

    fix build error

--- src/components/header.js
+++ src/components/header.js
@@ -11,19 +11,20 @@ class Header extends React.Component {
       <GlobalStateContext.Consumer>
         {(globalState) => { 
           const hasRootFolder = globalState.user && globalState.user.rootFolder;
+          const rootFolder = globalState.user ? globalState.user.rootFolder : "";
           const noUserElement = <p className="is-6 has-text-danger subtitle">Download our <a href="#">Automatic Mod Installer</a>, or turn it on to <Link to="/second">supercharge</Link> the website!</p>;
           const hasRootFolderElement = <p className="is-6 has-text-success subtitle">Risk of Rain 2 {globalState.user && globalState.user.has_bepin && 'and BepInEx'} located at <a onClick={() => {
-            const newPath = prompt('Enter the path to the folder where Risk of Rain 2.exe is found.\nThis is located in your Steam folder under "steamapps/common".', globalState.user.rootFolder || "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2");
+            const newPath = prompt('Enter the path to the folder where Risk of Rain 2.exe is found.\nThis is located in your Steam folder under "steamapps/common".', rootFolder || "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2");
             if (newPath) {
               globalState.updateRootFolder(newPath)
             }
-          }}>{globalState.user.rootFolder || ""}</a></p>;
+          }}>{rootFolder || ""}</a></p>;
           const missingRootFolderElement = <p className="is-6 has-text-danger subtitle">Risk of Rain 2 cannot be located. We checked <a onClick={() => {
-            const newPath = prompt('Enter the path to the folder where Risk of Rain 2.exe is found.\nThis is located in your Steam folder under "steamapps/common".', globalState.user.rootFolder || "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2");
+            const newPath = prompt('Enter the path to the folder where Risk of Rain 2.exe is found.\nThis is located in your Steam folder under "steamapps/common".', rootFolder || "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2");
             if (newPath) {
               globalState.updateRootFolder(newPath)
             }
-          }}>{globalState.user.rootFolder || "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2"}</a></p>;
+          }}>{rootFolder || "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2"}</a></p>;
           return (
           <header style={{
             display: "flex",
